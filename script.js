f4e8e285a19da8157c8ab83057d82d784f214735
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-06-25 17:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+ // Check if there are saved login details
+    var savedUsername = localStorage.getItem('username');
+    var savedPassword = localStorage.getItem('password');
+
+    if (savedUsername && savedPassword) {
+      var loginButton = document.createElement('button');
+      loginButton.setAttribute('id', 'existing');
+      loginButton.textContent = 'Login as existing user';
+      loginButton.addEventListener('click', function() {
+        alert('Logged in as ' + savedUsername);
+      });
+      document.body.appendChild(loginButton);
+    }
+
+ // Handle form submission
+    var loginForm = document.getElementById('login-form');
+    loginForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      var username = document.getElementById('username').value;
+      var password = document.getElementById('password').value;
+      var rememberMe = document.getElementById('checkbox').checked;
+
+      if (rememberMe) {
+        // Save login details to local storage
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+      } else {
+        // Remove login details from local storage
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+      }
+
+      alert('Logged in as ' + username);
+    });
